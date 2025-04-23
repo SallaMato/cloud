0dd8fd1dcda485d2e97cc90146a78f927dd0dee1
SallaMato <salla.rokkonen@gmail.com>
2025-04-23 11:47:15 +0300

    korjauksia

--- bundle_f/public/app.js
+++ bundle_f/public/app.js
@@ -1,6 +1,6 @@
 document.getElementById("hae").addEventListener("click", async () => {
     try {
-        const response = await fetch("https://<TÄHÄN-YOUR-RENDER-URL>.onrender.com/tapahtumat");
+        const response = await fetch("https://https://historia-backend-0yu4.onrender.com/tapahtumat");
         const data = await response.json();
         document.getElementById("tulokset").innerHTML = data.tapahtumat;
     } catch (error) {
